ab27cce6837c8f6bb7e80385db20664990581882
German Santos <santos.german@gmail.com>
2023-10-11 18:45:53 -0700

    Add mode and cart feature

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,22 @@
 import React from "react";
 import Item from "./Item";
+import { useState } from "react";
 
 function ShoppingList({ items }) {
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  //console.log(selectedCategory);
+
+  function handleSelect(e) {
+    console.log(e.target.value);
+    setSelectedCategory(e.target.value);
+    //console.log(selectedCategory);
+  }
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleSelect}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -22,3 +33,41 @@ function ShoppingList({ items }) {
 }
 
 export default ShoppingList;
+
+// {
+//   cart ? "" : "in-cart";
+// }
+
+// import React from "react";
+// import Item from "./Item";
+// import { useState } from "react";
+
+// function ShoppingList({ items }) {
+//   const [selectedCategory, setSelectedCategory] = useState("All");
+
+//   console.log(selectedCategory);
+
+//   function handleSelect(e) {
+//     console.log(e.target.value);
+//   }
+
+//   return (
+//     <div className="ShoppingList">
+//       <div className="Filter">
+//         <select name="filter" onChange={handleSelect}>
+//           <option value="All">Filter by category</option>
+//           <option value="Produce">Produce</option>
+//           <option value="Dairy">Dairy</option>
+//           <option value="Dessert">Dessert</option>
+//         </select>
+//       </div>
+//       <ul className="Items">
+//         {items.map((item) => (
+//           <Item key={item.id} name={item.name} category={item.category} />
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+
+// export default ShoppingList;
