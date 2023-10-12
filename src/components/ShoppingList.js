789b78a5aae8b07ea099183c06e72136b13f607b
German Santos <santos.german@gmail.com>
2023-10-11 23:38:28 -0700

    Add selection filter

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -5,13 +5,16 @@ import { useState } from "react";
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
 
-  //console.log(selectedCategory);
-
   function handleSelect(e) {
     console.log(e.target.value);
     setSelectedCategory(e.target.value);
     //console.log(selectedCategory);
   }
+
+  const renderList =
+    selectedCategory === "All"
+      ? items
+      : items.filter((item) => item.category === selectedCategory);
 
   return (
     <div className="ShoppingList">
@@ -24,7 +27,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {renderList.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
@@ -33,41 +36,3 @@ function ShoppingList({ items }) {
 }
 
 export default ShoppingList;
-
-// {
-//   cart ? "" : "in-cart";
-// }
-
-// import React from "react";
-// import Item from "./Item";
-// import { useState } from "react";
-
-// function ShoppingList({ items }) {
-//   const [selectedCategory, setSelectedCategory] = useState("All");
-
-//   console.log(selectedCategory);
-
-//   function handleSelect(e) {
-//     console.log(e.target.value);
-//   }
-
-//   return (
-//     <div className="ShoppingList">
-//       <div className="Filter">
-//         <select name="filter" onChange={handleSelect}>
-//           <option value="All">Filter by category</option>
-//           <option value="Produce">Produce</option>
-//           <option value="Dairy">Dairy</option>
-//           <option value="Dessert">Dessert</option>
-//         </select>
-//       </div>
-//       <ul className="Items">
-//         {items.map((item) => (
-//           <Item key={item.id} name={item.name} category={item.category} />
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// }
-
-// export default ShoppingList;
